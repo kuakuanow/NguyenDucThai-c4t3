934ac3051b1efd384b066a0e6cbdda7a7a42a4b1
kuakuanow <39945659+kuakuanow@users.noreply.github.com>
2018-07-11 18:40:10 +0700

    Add files via upload

---
+++ maps - Copy/dirt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dirt" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../images/sprite/dirt.png" width="32" height="32"/>
+</tileset>
